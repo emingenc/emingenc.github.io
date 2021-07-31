2cf82e5dfde127592440e224264090306b43ba3c
emingenc <emingenc@users.noreply.github.com>
2021-07-31 09:19:44 +0000

    deploy: 4b5fbeb4f7f8d725d11c3eda47c546fb0a8bb18b

--- _nuxt/static/1627722822/blog/gonderi/nedir-bu-blog/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/blog/gonderi/nedir-bu-blog", {data:[{_img:{}}],fetch:{"0":{_error:{message:"\u002Fblog-yenilendi not found",statusCode:500}}},mutations:void 0});
---
+++ _nuxt/static/1627723181/blog/post/blogun-baslica-ozellikleri/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/blog/post/blogun-baslica-ozellikleri", {data:[{_img:{}}],fetch:{"0":{_error:{message:"\u002Fneler-donuyor not found",statusCode:500}}},mutations:void 0});
