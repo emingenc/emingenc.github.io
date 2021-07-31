f2698fa881e497932fb96340a0d16d851e7b067f
emingenc <emingenc@novit.ai>
2021-07-31 11:41:43 +0300

    FIX: blog navbar menu logo fixed

--- src/components/Navbar/Blog.vue
+++ src/components/Navbar/Blog.vue
@@ -9,7 +9,7 @@
       >
         <SmartImage
           src="/assets/icons/icon.svg"
-          class="rounded-md bg-gray-100 ring-black transition-transform ring-2 ring-opacity-5 sm:transform hover:-rotate-6"
+          class="rounded-md ring-black transition-transform ring-2 ring-opacity-5 sm:transform hover:-rotate-6"
           width="100"
           height="100"
           :title="getTargetRoute.title"
