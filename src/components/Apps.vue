1c02900721c2678cff083c667233090a8d7f31ca
emingenc <emingenc@novit.ai>
2021-08-01 20:26:10 +0300

    FIX: minor fixes - footer, repo filter

--- src/components/Apps.vue
+++ src/components/Apps.vue
@@ -39,22 +39,7 @@ interface App {
   to?: string
   image?: string
 }
-
-axios.defaults.headers.common['header1'] =  {
-      headers: {
-            "authority": "play.google.com",
-            "cache-control": "max-age=0",
-            "sec-ch-ua": "\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"",
-            "sec-ch-ua-mobile": "?0",
-            "upgrade-insecure-requests": "1",
-            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            "x-client-data": "CLO1yQEIjbbJAQiktskBCMS2yQEIqZ3KAQiPucoBCJ2MywEIjJ7LAQigoMsBCNzyywEI7/LLAQjO9ssBCLP4ywEInvnLAQjw+csBCI/6ywEIr/rLAQjv+ssBGLryywEYj/XLAQ==",
-            "sec-fetch-site": "none",
-            "sec-fetch-mode": "navigate",
-            "sec-fetch-user": "?1",
-            "sec-fetch-dest": "document",
-        }}     
+    
 
 
 
@@ -68,7 +53,22 @@ export default Vue.extend({
       
     async function fetchHTML(url : string) {
      const { data } = await axios.get(
-        url
+        url,{
+          headers: {
+            "authority": "play.google.com",
+            "cache-control": "max-age=0",
+            "sec-ch-ua": "\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"",
+            "sec-ch-ua-mobile": "?0",
+            "upgrade-insecure-requests": "1",
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "x-client-data": "CLO1yQEIjbbJAQiktskBCMS2yQEIqZ3KAQiPucoBCJ2MywEIjJ7LAQigoMsBCNzyywEI7/LLAQjO9ssBCLP4ywEInvnLAQjw+csBCI/6ywEIr/rLAQjv+ssBGLryywEYj/XLAQ==",
+            "sec-fetch-site": "none",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-user": "?1",
+            "sec-fetch-dest": "document",
+        }
+        }
       )
       return cheerio.load(data)
     }
